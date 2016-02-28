c791988752b9ca5ce3e559a652a1d4acfd5b41b0
James <jamesizhou@gmail.com>
2016-02-27 20:31:22 -0800

    James

---
+++ TranslinkStopNote/app/src/main/java/fourasians/translinkstopnote/MainActivity.java
@@ -0,0 +1,47 @@
+package fourasians.translinkstopnote;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button) this.findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+
+                                 @Override
+                                 public void onClick(View v) {
+
+                                     Intent i = new Intent(MainActivity.this, SecondActivity.class);
+
+
+                                 }
+                             }
+        );
+
+
+    }
+}
